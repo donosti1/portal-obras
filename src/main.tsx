451b9e9ffa7c0493858ee2590e34f3d1f24b70bb
Donosti <saadorazio@gmail.com>
2021-07-19 00:01:03 -0300

    gh-pages fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {ChakraProvider, ColorModeScript} from "@chakra-ui/react";
 
 import App from "./app";
@@ -10,11 +9,11 @@ import theme from "./theme";
 ReactDOM.render(
   <>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <BrowserRouter>
+    <HashRouter basename="/portal-obras/">
       <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
-    </BrowserRouter>
+    </HashRouter>
   </>,
   document.getElementById("root"),
 );
